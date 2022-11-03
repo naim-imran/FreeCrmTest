95fac68e6f72e1a110036d77e7edad9925529e7c
Naim Imran <naim.nrb@gmail.com>
2022-11-03 05:39:17 -0400

    ContactPage class added

--- src/main/java/com/crm/qa/pages/ContactsPage.java
+++ src/main/java/com/crm/qa/pages/ContactsPage.java
@@ -1,5 +1,27 @@
 package com.crm.qa.pages;
 
-public class ContactsPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.crm.qa.initialComponents.InitialSetups;
+
+public class ContactsPage extends InitialSetups{
+	
+	@FindBy(xpath="//td[contains(text(),'Contacts')]")
+	WebElement contactsLabel;
+	
+	public ContactsPage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+	public boolean verifyContactsLabel() {
+		return contactsLabel.isDisplayed();
+	}
+	
+	public void selectContactCheckBox(String name) {
+		driver.findElement(By.xpath("//a[contains(text(),'"+name+"')]//parent::td[@class='datalistrow']//preceding-sibling::td[@class='datalistrow']//input[@type='checkbox']")).click();
+	}
 
 }
